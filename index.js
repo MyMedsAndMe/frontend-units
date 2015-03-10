4f780d71f040be2384c08a90692bd9a1599924de
paulrogers909 <paulrogers909@gmail.com>
2015-03-10 22:59:52 +0000

    test added

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 export default class Storage {
 
-  store() {
-  }
+    constructor() {
+        this.mymap = new Map();
+    }
 
-  get() {
-  }
+    store(k, v) {
+        if (typeof k === 'object') {
+            var obj = k;
+            for (var prop in obj) {
+                this.mymap.set(prop, obj[prop]);
+            }
+        } else {
+            this.mymap.set(k, v);
+        }
+    }
 
+    get(k) {
+        if (typeof k === 'undefined') {
+            var obj = {};
+            for (var [k, v] of this.mymap.entries()) {
+                obj[k] = v;
+            }
+            return obj;
+        }
+        if (this.mymap.has(k)) {
+            return this.mymap.get(k);
+        }
+    }
 }
